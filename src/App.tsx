92f438fe8779b5e8e5bad11aa12515281b461c93
Szymon Studniarz <szymek1019@gmail.com>
2022-09-29 16:03:06 +0200

    add get token method, use react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import React from "react";
 import "./App.css";
 import { Login } from "./views/Login/Login";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { Credentials } from "./backendTypes";
+import axios from "axios";
+
+const getToken = async ({ username, password }: Credentials) => {
+  const response = await axios.post(
+    `${process.env.REACT_APP_URL}/api/login`,
+    { username: username, password: password },
+    {
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
+  if (response.status === 200) {
+    localStorage.setItem("token", response.data.token);
+  }
+};
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login onLogin={getToken} />,
+  },
+]);
 
 function App() {
-  return <Login />;
+  console.log(localStorage.getItem("token"));
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;

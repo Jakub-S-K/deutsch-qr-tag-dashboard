66c615d7b58309097990901b33f22008534b1860
Szymon Studniarz <szymek1019@gmail.com>
2022-10-22 20:42:17 +0200

    create loader with GET, POST request types, redirect when unauthorized

---
+++ src/utilities.ts
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { redirect } from "react-router-dom";
+import { User } from "./backendTypes";
+
+export const loader = async ({
+  path,
+  requestType = "GET",
+}: {
+  path: string;
+  requestType?: "GET" | "POST";
+}) => {
+  if (requestType === "GET") {
+    try {
+      const response = await axios.get<User[]>(
+        `${process.env.REACT_APP_URL}/${path}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "content-type": "application/json",
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return redirect("/login");
+      } else {
+        console.log(error);
+      }
+    }
+  }
+  if (requestType === "POST") {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_URL}/${path}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "content-type": "application/json",
+          },
+        }
+      );
+      if (response.status === 401) {
+        return redirect("/login");
+      }
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return redirect("/login");
+      } else {
+        console.log(error);
+      }
+    }
+  }
+};

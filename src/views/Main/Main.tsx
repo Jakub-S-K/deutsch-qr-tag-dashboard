673148d34a04b0741b9637f290d765d9ea529f24
Szymon Studniarz <szymek1019@gmail.com>
2022-10-23 16:26:40 +0200

    update main view

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -29,9 +29,32 @@ export const Main = () => {
           placeholder="Description..."
         />
       </InputGroup>
-      <Button color="primary" outline className="px-5 mt-5">
+      <Button color="primary" outline className="px-5 my-3">
         Apply changes
       </Button>
+      <h3>User management</h3>
+      <hr className="vertical-line" />
+      <div className="d-flex flex-row justify-content-evenly">
+        <Button color="primary" outline className="px-5 mx-3">
+          Leaderboard
+        </Button>
+        <Button color="warning" outline className="px-5 mx-3">
+          Manage
+        </Button>
+        <Button color="success" className="px-5 mx-3">
+          Add user
+        </Button>
+      </div>
+      <h3>Questions management</h3>
+      <hr className="vertical-line" />
+      <div className="d-flex flex-row justify-content-between">
+        <Button color="warning" outline className="px-5 mx-3">
+          Manage
+        </Button>
+        <Button color="success" className="px-5 mx-3">
+          Add question
+        </Button>
+      </div>
     </>
   );
 };

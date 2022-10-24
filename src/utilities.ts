93f1c7324ad22204aabaf477231c66e6aec8b977
Szymon Studniarz <szymek1019@gmail.com>
2022-10-24 17:20:46 +0200

    add validate method which redirects user from login page (WIP)

--- src/utilities.ts
+++ src/utilities.ts
@@ -54,3 +54,32 @@ export const loader = async ({
     }
   }
 };
+
+export const validate = async ({
+  path = "api/users",
+  requestType = "GET",
+}: {
+  path?: string;
+  requestType?: "GET" | "POST";
+}) => {
+  if (requestType === "GET") {
+    try {
+      const response = await axios.get<User[]>(
+        `${process.env.REACT_APP_URL}/${path}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "content-type": "application/json",
+          },
+        }
+      );
+      return redirect("/");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return redirect("/login");
+      } else {
+        console.log(error);
+      }
+    }
+  }
+};

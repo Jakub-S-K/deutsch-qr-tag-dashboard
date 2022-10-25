f0fd082505432417bc5126090215ece503d1f882
Szymon Studniarz <szymek1019@gmail.com>
2022-10-25 14:42:45 +0200

    add userForm view which handles adding new and editing existing user automatically fill fields when editing user display appropriate button style according to function it serves

---
+++ src/views/Users/UserForm.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { Button, Input, Label, Table } from "reactstrap";
+import { mdiDelete, mdiPencil } from "@mdi/js";
+import Icon from "@mdi/react";
+import { User } from "../../backendTypes";
+import { loader } from "../../utilities";
+import { useLoaderData } from "react-router-dom";
+
+export const UserForm = () => {
+  let userData: any = useLoaderData();
+  let userModify = true;
+  if (!userData) {
+    userData = { name: "", surname: "" };
+    userModify = false;
+  }
+  const [user, setUser] = useState(userData);
+  const [status, setStatus] = useState(-1);
+  const handleChange = (property: "name" | "surname", value: string) => {
+    setUser({ ...user, [property]: value });
+  };
+  console.log(`${userData.surname} -> ${user.surname}`);
+  return (
+    <>
+      {status === 200 ? (
+        <div
+          color="success"
+          className="border border-2 border-success rounded text-center text-success"
+        >
+          Zapisano pomyślnie!
+        </div>
+      ) : null}
+      <Label for="name">Imię</Label>
+      <Input
+        id="name"
+        value={user.name}
+        onChange={(e) => {
+          handleChange("name", e.target.value);
+        }}
+        onFocus={(e) =>
+          e.currentTarget.setSelectionRange(
+            e.currentTarget.value.length,
+            e.currentTarget.value.length
+          )
+        }
+      ></Input>
+      <Label for="surname">Nazwisko</Label>
+      <Input
+        id="surname"
+        value={user.surname}
+        onChange={(e) => {
+          handleChange("surname", e.target.value);
+        }}
+        onFocus={(e) =>
+          e.currentTarget.setSelectionRange(
+            e.currentTarget.value.length,
+            e.currentTarget.value.length
+          )
+        }
+        className="mb-2"
+      ></Input>
+      {userModify ? (
+        <Button
+          color="primary"
+          onClick={async () =>
+            setStatus(
+              await loader({
+                path: `api/user/${user._id}`,
+                requestType: "PATCH",
+                payload: { name: user.name, surname: user.surname },
+              })
+            )
+          }
+        >
+          Zapisz
+        </Button>
+      ) : (
+        <Button
+          color="success"
+          outline
+          onClick={() =>
+            loader({
+              path: "api/user",
+              requestType: "POST",
+              payload: { name: user.name, surname: user.surname },
+            })
+          }
+        >
+          Dodaj użytkownika
+        </Button>
+      )}
+    </>
+  );
+};

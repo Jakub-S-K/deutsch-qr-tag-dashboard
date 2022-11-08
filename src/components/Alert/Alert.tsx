54f8c405f12e1dd6e86eec9cd56e33cc9757fcf9
Szymon Studniarz <szymek1019@gmail.com>
2022-11-08 19:48:17 +0100

    color depends on status, default messages overwritable by children

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,23 +1,48 @@
 import React, { useContext } from "react";
-import { Button, Alert as AlertBootstrap } from "reactstrap";
+import { Toast, CloseButton } from "reactstrap";
+import cx from "classnames";
 import { AlertContext } from "../../contexts";
+import { responseStatus } from "../../backendTypes";
 export const Alert = ({
-  isOpen = true,
   children,
 }: {
   isOpen?: boolean;
   children?: string;
 }) => {
   const value = useContext(AlertContext);
+
+  const getMessage = (status: responseStatus) => {
+    switch (status) {
+      case responseStatus.SUCCESS:
+        return "Wykonano pomyślnie.";
+      case responseStatus.ERR_BAD_REQUEST:
+        return "Wystąpił błąd. Sprawdź poprawność danych i spróbuj ponownie.";
+      case responseStatus.ERR_UNAUTHORIZED:
+        return "Sesja wygasła. Zaloguj się ponownie, aby kontynuować.";
+      case responseStatus.ERR_NOT_FOUND:
+        return "Wystąpił błąd. Nie znaleziono szukanego zasobu.";
+      case responseStatus.ERR_ALREADY_EXISTS:
+        return "Wystąpił błąd. Ten zasób już istnieje.";
+      case responseStatus.ERR_INTERNAL:
+        return "Wystąpił błąd. Prosimy spróbować później.";
+    }
+  };
   return (
-    <AlertBootstrap color="success">
+    <Toast
+      role={"alert"}
+      className={cx("text-white", {
+        "bg-success": value.alert === responseStatus.SUCCESS,
+        "bg-danger": value.alert >= 400,
+      })}
+      isOpen={value.alert !== responseStatus.NO_ALERT}
+    >
       <>
-        {alert}
-        {children}{" "}
-        <Button onClick={() => value.setAlert(value.alert + 1)}>
-          <>dodaj =&gt; {value.alert}</>
-        </Button>
+        {typeof children !== "undefined" ? children : getMessage(value.alert)}
+        <CloseButton
+          variant="white"
+          onClick={() => value.setAlert(responseStatus.NO_ALERT)}
+        />
       </>
-    </AlertBootstrap>
+    </Toast>
   );
 };

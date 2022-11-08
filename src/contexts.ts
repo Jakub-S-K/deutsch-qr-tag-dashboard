556244365b8be41fe75e7510ddd421323f7d95ec
Szymon Studniarz <szymek1019@gmail.com>
2022-11-08 19:52:38 +0100

    add autohide alert

--- src/contexts.ts
+++ src/contexts.ts
@@ -1,11 +1,20 @@
 import { createContext, useState } from "react";
+import { responseStatus } from "./backendTypes";
 
 export const useValue = () => {
-  const [alert, setAlert] = useState(0);
+  const [alert, setAlert] = useState<responseStatus>(responseStatus.SUCCESS);
+
+  const alertAndDismiss = (status: responseStatus, time: number = 2000) => {
+    setAlert(status);
+    setInterval(() => {
+      setAlert(responseStatus.NO_ALERT);
+    }, time);
+  };
 
   return {
     alert,
     setAlert,
+    alertAndDismiss,
   };
 };
 export const AlertContext = createContext({} as ReturnType<typeof useValue>);

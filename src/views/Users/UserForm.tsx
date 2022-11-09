8cacccea9f1503d159f7994560b10011c8b9501f
Szymon Studniarz <szymek1019@gmail.com>
2022-11-09 20:36:43 +0100

    dispatch request with enter, automatically focus on name input

--- src/views/Users/UserForm.tsx
+++ src/views/Users/UserForm.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Alert, Button, Input, Label, Table } from "reactstrap";
-import { mdiDelete, mdiPencil } from "@mdi/js";
-import Icon from "@mdi/react";
-import { User } from "../../backendTypes";
+import React, { useEffect, useState, useRef } from "react";
+import { Alert, Button, Input, Label } from "reactstrap";
 import { patchRequest, postRequest } from "../../utilities";
 import { Retreat } from "../../components/Retreat/Retreat";
 import { useLoaderData } from "react-router-dom";
@@ -15,6 +12,7 @@ export const UserForm = () => {
     userModify = false;
   }
   const [user, setUser] = useState(userData);
+  const nameInput = useRef<HTMLInputElement>(null);
   const [status, setStatus] = useState<number | undefined>(-1);
   useEffect(() => {
     setTimeout(() => {
@@ -24,76 +22,91 @@ export const UserForm = () => {
   const handleChange = (property: "name" | "surname", value: string) => {
     setUser({ ...user, [property]: value });
   };
+  const modifyUserAction = async () => {
+    setStatus(
+      await patchRequest({
+        path: `api/user/${user._id}`,
+        payload: { name: user.name, surname: user.surname },
+      })
+    );
+    userData.name = user.name;
+    userData.surname = user.surname;
+  };
+  const addUserAction = async () => {
+    setStatus(
+      await postRequest({
+        path: "api/user",
+        payload: { name: user.name, surname: user.surname },
+      })
+    );
+    setUser({ ...user, name: "", surname: "" });
+  };
   console.log(`${userData.surname} -> ${user.surname}`);
   return (
     <>
       <Retreat />
-      {status === 200 ? <Alert>Zapisano pomyślnie</Alert> : null}
-      <Label for="name">Imię</Label>
-      <Input
-        id="name"
-        value={user.name}
-        onChange={(e) => {
-          handleChange("name", e.target.value);
-        }}
-        onFocus={(e) =>
-          e.currentTarget.setSelectionRange(
-            e.currentTarget.value.length,
-            e.currentTarget.value.length
-          )
-        }
-      ></Input>
-      <Label for="surname">Nazwisko</Label>
-      <Input
-        id="surname"
-        value={user.surname}
-        onChange={(e) => {
-          handleChange("surname", e.target.value);
-        }}
-        onFocus={(e) =>
-          e.currentTarget.setSelectionRange(
-            e.currentTarget.value.length,
-            e.currentTarget.value.length
-          )
-        }
-        className="mb-2"
-      ></Input>
-      {userModify ? (
-        <Button
-          color="primary"
-          disabled={
-            user.name === userData.name && user.surname === userData.surname
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          if (userModify) {
+            modifyUserAction();
+          } else {
+            addUserAction();
           }
-          onClick={async () => {
-            setStatus(
-              await patchRequest({
-                path: `api/user/${user._id}`,
-                payload: { name: user.name, surname: user.surname },
-              })
-            );
-            userData.name = user.name;
-            userData.surname = user.surname;
+          setTimeout(() => nameInput.current!.focus(), 100);
+        }}
+      >
+        {status === 200 ? <Alert>Zapisano pomyślnie</Alert> : null}
+        <Label for="name">Imię</Label>
+        <Input
+          id="name"
+          value={user.name}
+          onChange={(e) => {
+            handleChange("name", e.target.value);
           }}
-        >
-          Zapisz
-        </Button>
-      ) : (
-        <Button
-          color="success"
-          outline
-          disabled={user.name.length === 0 || user.surname.length === 0}
-          onClick={async () =>
-            setStatus(
-              await postRequest({
-                path: "api/user",
-                payload: { name: user.name, surname: user.surname },
-              })
+          innerRef={nameInput}
+          onFocus={(e) =>
+            e.currentTarget.setSelectionRange(
+              e.currentTarget.value.length,
+              e.currentTarget.value.length
             )
           }
-        >
-          Dodaj użytkownika
-        </Button>
-      )}
+          autoFocus
+        ></Input>
+        <Label for="surname">Nazwisko</Label>
+        <Input
+          id="surname"
+          value={user.surname}
+          onChange={(e) => {
+            handleChange("surname", e.target.value);
+          }}
+          onFocus={(e) =>
+            e.currentTarget.setSelectionRange(
+              e.currentTarget.value.length,
+              e.currentTarget.value.length
+            )
+          }
+          className="mb-2"
+        ></Input>
+        {userModify ? (
+          <Button
+            color="primary"
+            disabled={
+              user.name === userData.name && user.surname === userData.surname
+            }
+          >
+            Zapisz
+          </Button>
+        ) : (
+          <Button
+            color="success"
+            outline
+            disabled={user.name.length === 0 || user.surname.length === 0}
+          >
+            Dodaj użytkownika
+          </Button>
+        )}
+      </form>
     </>
   );
 };

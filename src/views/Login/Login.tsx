5c822a6082687e39ca170a629e38e55951e35683
Szymon Studniarz <szymek1019@gmail.com>
2022-11-09 10:14:41 +0100

    redirect to login page when unauthorized, display alert, mark inputs

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import { InputGroup, InputGroupText, Input, Button } from "reactstrap";
 import { mdiAccount, mdiLock } from "@mdi/js";
 import Icon from "@mdi/react";
+import { AlertContext } from "../../contexts";
+import { responseStatus } from "../../backendTypes";
+import { getToken } from "../../utilities";
 
-export const Login = ({ onLogin }: { onLogin: Function }) => {
+export const Login = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const loaderData = useLoaderData();
+  const alert = useContext(AlertContext);
+  useEffect(() => {
+    if (loaderData === responseStatus.SUCCESS) {
+      alert.alertAndDismiss(responseStatus.SUCCESS);
+      navigate("/");
+    }
+  }, []);
   return (
     <div
       className="container d-flex flex-column justify-content-center align-items-center border rounded p-5"
@@ -22,6 +31,7 @@ export const Login = ({ onLogin }: { onLogin: Function }) => {
         </InputGroupText>
         <Input
           placeholder="username"
+          invalid={alert.alert === responseStatus.ERR_NOT_FOUND}
           onChange={(e) => setUsername(e.target.value)}
         />
       </InputGroup>
@@ -32,15 +42,30 @@ export const Login = ({ onLogin }: { onLogin: Function }) => {
         <Input
           type="password"
           placeholder="password"
+          invalid={
+            alert.alert === responseStatus.ERR_BAD_REQUEST ||
+            alert.alert === responseStatus.ERR_NOT_FOUND
+          }
           onChange={(e) => setPassword(e.target.value)}
         />
       </InputGroup>
       <Button
         color="primary"
         className="px-5 mt-5"
-        onClick={() => {
-          navigate("/");
-          return onLogin({ username, password });
+        disabled={username.length === 0 || password.length === 0}
+        onClick={async () => {
+          const status = await getToken({ username, password });
+          if (status === responseStatus.SUCCESS) {
+            alert.alertAndDismiss(responseStatus.SUCCESS);
+            navigate("/");
+          } else {
+            if (status === responseStatus.ERR_BAD_REQUEST) {
+              alert.alertAndDismiss(responseStatus.ERR_BAD_REQUEST);
+            }
+            if (status === responseStatus.ERR_NOT_FOUND) {
+              alert.alertAndDismiss(responseStatus.ERR_NOT_FOUND);
+            }
+          }
         }}
       >
         Log in

d77b3b09ac6d5b6a6586ebe6a1beb3d013fdb8ad
Szymon Studniarz <szymek1019@gmail.com>
2022-11-29 19:48:14 +0100

    move routes to separate file, give Alert component access to router data

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,20 @@
 import React from "react";
 import "./App.css";
-import { Login } from "./views/Login/Login";
-import { Main } from "./views/Main/Main";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { loader, validate } from "./utilities";
-import { Users as UsersPage } from "./views/Users/Users";
-import { UserForm } from "./views/Users/UserForm";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { Alert } from "./components/Alert/Alert";
 import { AlertContext, useValue } from "./contexts";
-import { QuestionForm } from "./views/Questions/QuestionForm";
-import { Questions } from "./views/Questions/Questions";
-import { NotFound } from "./views/NotFound";
-import { Protected } from "./components/Protected/Protected";
+import { routes } from "./routes";
 
 const router = createBrowserRouter([
   {
-    path: "*",
-    element: <NotFound />,
-  },
-  {
-    path: "/login",
-    loader: () => validate({}),
-    element: <Login />,
-  },
-  {
     path: "/",
-    element: <Protected />,
-    children: [
-      {
-        path: "/",
-        index: true,
-        element: <Main />,
-      },
-      {
-        path: "users",
-        loader: () => loader({ path: "api/users" }),
-        element: <UsersPage />,
-      },
-      {
-        path: "users/:id",
-        loader: ({ params }) => loader({ path: `api/user/${params.id}` }),
-        element: <UserForm />,
-      },
-      {
-        path: "add/user",
-        element: <UserForm />,
-      },
-      {
-        path: "questions",
-        loader: () => loader({ path: "api/questions" }),
-        element: <Questions />,
-      },
-      {
-        path: "questions/:id",
-        loader: ({ params }) => loader({ path: `api/question/${params.id}` }),
-        element: <QuestionForm />,
-      },
-      {
-        path: "/add/question",
-        element: <QuestionForm />,
-      },
-    ],
+    element: (
+      <>
+        <Alert />
+        <Outlet />
+      </>
+    ),
+    children: routes,
   },
 ]);
 
@@ -68,7 +22,6 @@ function App() {
   return (
     <AlertContext.Provider value={useValue()}>
       <div className="content container">
-        <Alert></Alert>
         <RouterProvider router={router} />
       </div>
     </AlertContext.Provider>
---
+++ src/routes.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Login } from "./views/Login/Login";
+import { Main } from "./views/Main/Main";
+import { QuestionForm } from "./views/Questions/QuestionForm";
+import { Questions } from "./views/Questions/Questions";
+import { NotFound } from "./views/NotFound";
+import { Protected } from "./components/Protected/Protected";
+import { RouteObject } from "react-router-dom";
+import { loader, validate } from "./utilities";
+import { Users as UsersPage } from "./views/Users/Users";
+import { UserForm } from "./views/Users/UserForm";
+
+export const routes: RouteObject[] = [
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+  {
+    path: "/login",
+    loader: () => validate({}),
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <Protected />,
+    children: [
+      {
+        path: "/",
+        index: true,
+        element: <Main />,
+      },
+      {
+        path: "users",
+        loader: () => loader({ path: "api/users" }),
+        element: <UsersPage />,
+      },
+      {
+        path: "users/:id",
+        loader: ({ params }) => loader({ path: `api/user/${params.id}` }),
+        element: <UserForm />,
+      },
+      {
+        path: "add/user",
+        element: <UserForm />,
+      },
+      {
+        path: "questions",
+        loader: () => loader({ path: "api/questions" }),
+        element: <Questions />,
+      },
+      {
+        path: "questions/:id",
+        loader: ({ params }) => loader({ path: `api/question/${params.id}` }),
+        element: <QuestionForm />,
+      },
+      {
+        path: "/add/question",
+        element: <QuestionForm />,
+      },
+    ],
+  },
+];

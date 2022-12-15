e8a1842815398a090c84798c73384279de434f2f
Szymon Studniarz <szymek1019@gmail.com>
2022-12-15 17:23:24 +0100

    use validate function from utilities

--- src/components/Protected/Protected.tsx
+++ src/components/Protected/Protected.tsx
@@ -1,41 +1,31 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
-import jwtDecode from "jwt-decode";
 import { useAlert } from "../../contexts";
-import { responseStatus, Token } from "../../backendTypes";
+import { responseStatus } from "../../backendTypes";
+import { validate } from "../../utilities";
 
 export const Protected = () => {
   const alert = useAlert();
   const [redirect, setRedirect] = useState(true);
   const navigate = useNavigate();
   useEffect(() => {
-    const authCheck = async () => {
-      if (!localStorage.getItem("token")) {
-        setRedirect(true);
-        alert.alertAndDismiss(responseStatus.ERR_UNAUTHORIZED, {
-          message: "Zaloguj się, aby kontynuować!",
-        });
-        navigate("/login");
-      } else {
-        try {
-          const { exp } = jwtDecode<Token>(localStorage.getItem("token")!);
-          if (Date.now() < exp * 1000) {
-            setRedirect(false);
-          } else {
-            setRedirect(true);
-            alert.alertAndDismiss(responseStatus.ERR_UNAUTHORIZED, {
-              message: "Sesja wygasła. Zaloguj się ponownie.",
-            });
-            localStorage.removeItem("token");
-            navigate("/login");
-          }
-        } catch (err) {
-          localStorage.removeItem("token");
-        }
-      }
-    };
-    authCheck();
+    const authStatus = validate();
+    if (authStatus === responseStatus.SUCCESS) {
+      setRedirect(false);
+    } else if (authStatus === responseStatus.ERR_INTERNAL) {
+      setRedirect(true);
+      alert.alertAndDismiss(responseStatus.ERR_UNAUTHORIZED, {
+        message: "Sesja wygasła. Zaloguj się ponownie.",
+      });
+      navigate("/login");
+    } else {
+      setRedirect(true);
+      alert.alertAndDismiss(responseStatus.ERR_UNAUTHORIZED, {
+        message: "Zaloguj się, aby kontynuować!",
+      });
+      navigate("/login");
+    }
   }, []);
 
   return redirect ? null : <Outlet />;

261c1a88263a69bd3a714b1f4f8e76a049758727
Szymon Studniarz <szymek1019@gmail.com>
2022-12-18 12:54:53 +0100

    add Team type with type guards

--- src/backendTypes.ts
+++ src/backendTypes.ts
@@ -15,6 +15,11 @@ export interface payloadQuestion {
 export interface Question extends payloadQuestion {
   _id: string;
 }
+export interface Team {
+  _id: string;
+  name: string;
+  members: User[];
+}
 export interface Response<T = any> {
   status: number;
   data?: T;
@@ -56,4 +61,17 @@ export const isQuestion = (obj: any): obj is Question => {
 };
 export const isQuestionArr = (obj: any): obj is Question[] => {
   return Array.isArray(obj) && obj.every((value) => isQuestion(value));
+};
+
+export const isTeam = (obj: any): obj is Team => {
+  return (
+    !!obj &&
+    "_id" in obj &&
+    "name" in obj &&
+    "members" in obj &&
+    isUserArr(obj.members)
+  );
+};
+export const isTeamArr = (obj: any): obj is Team[] => {
+  return Array.isArray(obj) && obj.every((value) => isTeam(value));
 };
